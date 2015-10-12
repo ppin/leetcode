b5f655cb728e292076d1555b1ac754e3b6b8055f
Gabriel Zhang <shuai.xyz@gmail.com>
2015-10-13 00:27:32 +0800

    add suggest method for Trie

--- src/main/java/com/pleasecode/leetcode/solutions/Trie.java
+++ src/main/java/com/pleasecode/leetcode/solutions/Trie.java
@@ -1,6 +1,8 @@
 package com.pleasecode.leetcode.solutions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -69,6 +71,38 @@ public class Trie {
         return true;
     }
 
+    public List<String> suggest(String prefix) {
+        List<String> result = new ArrayList<>();
+        if (prefix == null || prefix.isEmpty()) {
+            return result;
+        }
+        TrieNode cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            cur = cur.children.get(prefix.charAt(i));
+            if (cur == null) {
+                return result;
+            }
+        }
+        StringBuilder builder = new StringBuilder(prefix);
+        helper(cur, builder, result);
+        return result;
+    }
+
+    private void helper(TrieNode cur, StringBuilder builder, List<String> result) {
+        if (cur == null) {
+            return;
+        }
+        if (cur.endOfWord) {
+            result.add(builder.toString());
+        }
+        for (char c : cur.children.keySet()) {
+            TrieNode node = cur.children.get(c);
+            builder.append(c);
+            helper(node, builder, result);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+
     class TrieNode {
         char value;
         boolean endOfWord;
@@ -87,5 +121,16 @@ public class Trie {
             this.endOfWord = endOfWord;
             children = new HashMap<>(26);
         }
+    }
+
+    public static void main(String[] args) {
+        final Trie trie = new Trie();
+        trie.insert("apple");
+        trie.insert("app");
+        trie.insert("apart");
+        trie.insert("application");
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("ap"));
+        System.out.println(trie.suggest("app"));
     }
 }
