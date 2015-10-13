6c5774f6901aa3c709f974f8578a4d9ebd64ad65
Gabriel Zhang <shuai.xyz@gmail.com>
2015-10-13 23:44:20 +0800

    optimize Two Sum

--- src/main/java/com/pleasecode/leetcode/solutions/TwoSum.java
+++ src/main/java/com/pleasecode/leetcode/solutions/TwoSum.java
@@ -9,19 +9,14 @@ import java.util.Map;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        int[] indices = new int[2];
-        if (nums == null || nums.length < 2) {
-            return indices;
-        }
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.get(nums[i]) == null) {
                 map.put(target - nums[i], i);
             } else {
-                indices[0] = map.get(nums[i]) + 1;
-                indices[1] = i + 1;
+                return new int[]{map.get(nums[i]) + 1, i + 1};
             }
         }
-        return indices;
+        throw new IllegalArgumentException("No two sum solution.");
     }
 }
