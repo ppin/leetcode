d6c9c28fa77df3022f99fa485c1b4d2bdb625edc
Gabriel Zhang <shuai.xyz@gmail.com>
2015-10-17 21:30:11 +0800

    add solution for Subset

---
+++ src/main/java/com/pleasecode/leetcode/solutions/Subset.java
@@ -0,0 +1,30 @@
+package com.pleasecode.leetcode.solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/subsets/
+ */
+public class Subset {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return subsets;
+        }
+        Arrays.sort(nums);
+        helper(nums, 0, new ArrayList<>(), subsets);
+        return subsets;
+    }
+
+    private void helper(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets) {
+        subsets.add(new ArrayList(subset));
+        for (int i = index; i< nums.length; i++) {
+            subset.add(nums[i]);
+            helper(nums, i + 1, subset, subsets);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
