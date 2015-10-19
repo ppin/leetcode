d3a89de30cdd1ff093879ee1e094072b05cce347
Gabriel Zhang <shuai.xyz@gmail.com>
2015-10-20 00:07:59 +0800

    unify the solution with II

--- src/main/java/com/pleasecode/leetcode/solutions/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/pleasecode/leetcode/solutions/RemoveDuplicatesFromSortedArray.java
@@ -6,16 +6,16 @@ package com.pleasecode.leetcode.solutions;
 public class RemoveDuplicatesFromSortedArray {
 
     public int removeDuplicates(int[] nums) {
-        int index = 0;
         if (nums == null || nums.length == 0) {
-            return index;
+            return 0;
         }
+        int index = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[index] == nums[i]) {
+            if (nums[index - 1] == nums[i]) {
                 continue;
             }
-            nums[++index] = nums[i];
+            nums[index++] = nums[i];
         }
-        return index + 1;
+        return index;
     }
 }
