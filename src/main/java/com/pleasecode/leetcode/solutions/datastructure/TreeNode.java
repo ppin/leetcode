55e82ed24f17c994032ae761381197ab5f95a5b6
Gabriel Zhang <shuai.xyz@gmail.com>
2015-10-20 00:20:37 +0800

    add data structure for Tree

---
+++ src/main/java/com/pleasecode/leetcode/solutions/datastructure/TreeNode.java
@@ -0,0 +1,12 @@
+package com.pleasecode.leetcode.solutions.datastructure;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
